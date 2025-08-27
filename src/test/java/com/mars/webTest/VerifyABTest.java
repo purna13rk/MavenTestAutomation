c69c362b25d0af5b7f9a66622e94af72e96bd96e
purna13rk <kollipurna.chandrareddy@gmail.com>
2025-08-27 21:29:52 +0530

    hello

--- src/test/java/com/mars/webTest/VerifyABTest.java
+++ src/test/java/com/mars/webTest/VerifyABTest.java
@@ -16,7 +16,7 @@ public class VerifyABTest extends BaseTest{
 	public void initialize() {
 		homePage = new HomePage(driver);
 	}
-	//Hello TestNG
+	//Hello TestNG hello
 	@Test
 	public void abTest() {
 		SoftAssert sa = new SoftAssert();
